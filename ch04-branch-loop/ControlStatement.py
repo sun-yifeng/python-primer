afb2ea5cc5260ad9b620b02b6a280c7fafb4faf0
sunyifeng <9339996@qq.com>
2018-11-07 17:49:20 +0800

    分支和循环

---
+++ ch04-branch-loop/ControlStatement.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2018/11/7 5:41 PM
+# @Author: sunyf
+# @File  : ControlStatement.py
+
+""" for循环  """
+favorite = "fish C"
+#遍历字符
+for each in favorite:
+    print(each, end=",")
+
+""" range() """
+for i in range(1, 10 ,2):
+    print(i)
+
+# break    无特殊之处
+# continue 无特殊之处
