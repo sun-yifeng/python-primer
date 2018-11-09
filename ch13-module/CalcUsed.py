fb495ba6a89bb2e7428dad9887265917999f5b97
sunyifeng <9339996@qq.com>
2018-11-09 16:46:40 +0800

    模块、包、搜索路径

---
+++ ch13-module/CalcUsed.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2018/11/9 4:33 PM
+# @Author: sunyf
+# @File  : CalcUsed.py
+
+
+# import pkg.calc as ts
+
+if __name__ == '__main__':
+    pass
+#    test()
+
+
+
---
+++ ch13-module/pkg/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time  : 2018/11/9 4:26 PM
+# @Author: sunyf
+# @File  : __init__.py.py
